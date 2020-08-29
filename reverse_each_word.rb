7c62444b6a7af90d70999953240003cbfa29b92b
Jocelyn  Ramirez <ramirez.jocelyn73@yahoo.com>
2020-08-29 07:13:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,11 +9,11 @@ end
 
 def reverse_each_word (word)
   word = word.split
-  new_array=[]
+    new_array=[]
   word.collect do |word|
     new_array << word.reverse
   end
-  new_array.join (" ")
+ new_array.join (" ")
     
     
 end
